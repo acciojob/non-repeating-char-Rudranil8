5efe7f1029bbb8efa0c7bec3054b68db057d6446
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-17 22:13:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	for(let a=0; a<str.length; a++){
+		let count=0;
+		for(let b=0; b<str.length; b++){
+			if(a==b){
+				continue;
+			}
+			if(str[a]==str[b]){
+				count++;
+			}
+		}
+		if(count==0){
+			return str[a];
+		}
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
